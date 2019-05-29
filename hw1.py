c148d4234d70d0aa291761e9b5c72aefb8a1ab4b
富木菜穂 <tomikinaho@tomikinaonoMacBook-Pro.local>
2019-05-29 13:59:32 +0900

    mainly change comments

--- hw1.py
+++ hw1.py
@@ -2,23 +2,23 @@ import copy
 import re
 import collections
 
-#import dictionary containing words whose word length is 16 or less
+# Import dictionary containing words whose word length is 16 or less
 dic_16 = []
 with open("dictionary.txt", 'r') as f:
     for line in f:
         line = line.replace("qu", "q")
         dic_16.append(line.replace("\n", ""))
 
-#make a small dictionary that contains words which is consisted of  only given characters
+# Make a small dictionary that contains words consisting only of the given characters
 def mk_new_dic(s):
     new_dic = []
-    match = ''.join(sorted(set(s)))
+    match = re.compile("[^"+s+"]")
     for v in dic_16:
-        if re.search("[^"+match+"]", v) == None:
+        if re.search(match, v) == None:
             new_dic.append(v)
     return new_dic
 
-#make a list of words which can be the answer
+# Make a list of words which only use the supplied letters.
 def mk_match(new_dic, s):
     counter = collections.Counter(s)
     l = []
@@ -27,6 +27,10 @@ def mk_match(new_dic, s):
             l.append(word)
     return l
 
+# Culculate the score for a word.
+# The score for a given word is the square of the sum of its letter points plus a bonus point for not passing
+# Letter point
+# 'jxqxz' : 3, 'cfhlmpvwy' : 2, others : 1 point for each.
 def point(word):
     if word == '':
         return 0
@@ -37,25 +41,25 @@ def point(word):
     count += 2 * len(c2)
     c1 = re.findall("[^jkqxzcfhlmpvwy]", word)
     count += len(c1)
-    return (count) ** 2
+    return count ** 2
 
 def mk_best_word(original):
     new_dic = mk_new_dic(original)
     word_list = mk_match(new_dic, original)
     if word_list == []:
         return "", 0
-    d = {}
+    score = {}
     for word in word_list:
-        d[word] = point(word)
-    d = sorted(d, key = lambda x : d[x], reverse = True)
-    return d[0], point(d[0])
+        score[word.replace("q", "qu")] = point(word)
+    score = sorted(score, key = lambda x : score[x], reverse = True)
+    return score[0], point(score[0])
 
-#receive16 characters
+# Interactively read 16 letters 10 times.
 score = 0
 for i in range(10):
     original = input().lower()
-    original = original.replace("qu", "q")
-    w, c = mk_best_word(original)
+    canonical = original.replace("qu", "q")
+    w, c = mk_best_word(canonical)
     score += c
     print(w, c)
-    
+print(score)
